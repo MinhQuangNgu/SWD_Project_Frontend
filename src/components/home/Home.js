f15cb4e0ec949cae5d778207f3560c00ad3ecf0c
Minh Quang <quangminhnguyen265@gmail.com>
2023-09-15 18:15:48 +0700

    updating

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Navigation, Pagination, Autoplay} from 'swiper/modules';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import './style.scss'
 const Home = () => {
@@ -430,7 +430,7 @@ const Home = () => {
             <p>Những đầu bếp có số lượng theo dõi lớn nhất với cũng cống hiến vô cùng lớn của họ.</p>
           </div>
           <Swiper
-            modules={[Navigation, Pagination,Autoplay]}
+            modules={[Navigation, Pagination, Autoplay]}
             spaceBetween={10}
             slidesPerView={3}
             navigation
@@ -438,61 +438,138 @@ const Home = () => {
             scrollbar={{ draggable: true }}
             loop
             autoplay={{
-            delay: 1000,
-            disableOnInteraction: false,
+              delay: 1000,
+              disableOnInteraction: false,
             }}
           >
             <SwiperSlide>
-              <div class="testimonial-item position-relative bg-white p-5 mt-4">
-                <i style={{color:"#f65005"}} class="fa fa-quote-left fa-3x position-absolute top-0 start-0 mt-n4 ms-5"></i>
-                <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>
-                <div class="d-flex align-items-center">
-                  <img style={{ width: "50px", height: "50px",objectFit:"cover" }} class="flex-shrink-0 rounded-circle" src="https://res.cloudinary.com/sttruyen/image/upload/v1694746217/another/rhadcgcifq2qjhhnsvob.jpg" alt="" />
-                  <div class="ms-3">
-                    <h5 class="mb-1">Client Name</h5>
-                    <span>Profession</span>
+              <section style={{backgroundColor:"#9de2ff",height:"300px",borderRadius:"30px"}}>
+                <div class="container py-5 h-100">
+                  <div class="row d-flex align-items-center h-100">
+                    <div class="col col-md-9 col-lg-7 col-xl-5">
+                      <div class="card" style={{borderRadius:"15px"}}>
+                        <div class="card-body p-4">
+                          <div class="d-flex text-black">
+                            <div class="flex-shrink-0">
+                              <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
+                                alt="Generic placeholder image" class="img-fluid"
+                                style={{width:"160px",height:"150px",objectFit:"cover"}} />
+                            </div>
+                            <div class="flex-grow-1 ms-3">
+                              <h5 class="mb-1">Danny McLoan</h5>
+                              <p class="mb-2 pb-1" style={{color:"#2b2a2a"}}>Senior Journalist</p>
+                              <div class="d-flex justify-content-start rounded-3 p-2 mb-2"
+                                style={{backgroundColor:"#efefef"}}>
+                                <div>
+                                  <p class="small text-muted mb-1">Articles</p>
+                                  <p class="mb-0">41</p>
+                                </div>
+                                <div class="px-3">
+                                  <p class="small text-muted mb-1">Followers</p>
+                                  <p class="mb-0">976</p>
+                                </div>
+                                <div>
+                                  <p class="small text-muted mb-1">Rating</p>
+                                  <p class="mb-0">8.5</p>
+                                </div>
+                              </div>
+                              <div class="d-flex pt-1">
+                                <button type="button" class="btn btn-primary flex-grow-1">Theo dõi</button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
+              </section>
             </SwiperSlide>
             <SwiperSlide>
-              <div class="testimonial-item position-relative bg-white p-5 mt-4">
-                <i style={{color:"#f65005"}} class="fa fa-quote-left fa-3x position-absolute top-0 start-0 mt-n4 ms-5"></i>
-                <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>
-                <div class="d-flex align-items-center">
-                  <img style={{ width: "50px", height: "50px",objectFit:"cover" }} class="flex-shrink-0 rounded-circle" src="https://res.cloudinary.com/sttruyen/image/upload/v1694746217/another/rhadcgcifq2qjhhnsvob.jpg" alt="" />
-                  <div class="ms-3">
-                    <h5 class="mb-1">Client Name</h5>
-                    <span>Profession</span>
+              <section style={{backgroundColor:"#9de2ff",height:"300px",borderRadius:"30px"}}>
+                <div class="container py-5 h-100">
+                  <div class="row d-flex align-items-center h-100">
+                    <div class="col col-md-9 col-lg-7 col-xl-5">
+                      <div class="card" style={{borderRadius:"15px"}}>
+                        <div class="card-body p-4">
+                          <div class="d-flex text-black">
+                            <div class="flex-shrink-0">
+                              <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
+                                alt="Generic placeholder image" class="img-fluid"
+                                style={{width:"160px",height:"150px",objectFit:"cover"}} />
+                            </div>
+                            <div class="flex-grow-1 ms-3">
+                              <h5 class="mb-1">Danny McLoan</h5>
+                              <p class="mb-2 pb-1" style={{color:"#2b2a2a"}}>Senior Journalist</p>
+                              <div class="d-flex justify-content-start rounded-3 p-2 mb-2"
+                                style={{backgroundColor:"#efefef"}}>
+                                <div>
+                                  <p class="small text-muted mb-1">Articles</p>
+                                  <p class="mb-0">41</p>
+                                </div>
+                                <div class="px-3">
+                                  <p class="small text-muted mb-1">Followers</p>
+                                  <p class="mb-0">976</p>
+                                </div>
+                                <div>
+                                  <p class="small text-muted mb-1">Rating</p>
+                                  <p class="mb-0">8.5</p>
+                                </div>
+                              </div>
+                              <div class="d-flex pt-1">
+                                <button type="button" class="btn btn-primary flex-grow-1">Theo dõi</button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
+              </section>
             </SwiperSlide>
             <SwiperSlide>
-              <div class="testimonial-item position-relative bg-white p-5 mt-4">
-                <i style={{color:"#f65005"}} class="fa fa-quote-left fa-3x position-absolute top-0 start-0 mt-n4 ms-5"></i>
-                <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>
-                <div class="d-flex align-items-center">
-                  <img style={{ width: "50px", height: "50px",objectFit:"cover" }} class="flex-shrink-0 rounded-circle" src="https://res.cloudinary.com/sttruyen/image/upload/v1694746217/another/rhadcgcifq2qjhhnsvob.jpg" alt="" />
-                  <div class="ms-3">
-                    <h5 class="mb-1">Client Name</h5>
-                    <span>Profession</span>
+              <section style={{backgroundColor:"#9de2ff",height:"300px",borderRadius:"30px"}}>
+                <div class="container py-5 h-100">
+                  <div class="row d-flex align-items-center h-100">
+                    <div class="col col-md-9 col-lg-7 col-xl-5">
+                      <div class="card" style={{borderRadius:"15px"}}>
+                        <div class="card-body p-4">
+                          <div class="d-flex text-black">
+                            <div class="flex-shrink-0">
+                              <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
+                                alt="Generic placeholder image" class="img-fluid"
+                                style={{width:"160px",height:"150px",objectFit:"cover"}} />
+                            </div>
+                            <div class="flex-grow-1 ms-3">
+                              <h5 class="mb-1">Danny McLoan</h5>
+                              <p class="mb-2 pb-1" style={{color:"#2b2a2a"}}>Senior Journalist</p>
+                              <div class="d-flex justify-content-start rounded-3 p-2 mb-2"
+                                style={{backgroundColor:"#efefef"}}>
+                                <div>
+                                  <p class="small text-muted mb-1">Articles</p>
+                                  <p class="mb-0">41</p>
+                                </div>
+                                <div class="px-3">
+                                  <p class="small text-muted mb-1">Followers</p>
+                                  <p class="mb-0">976</p>
+                                </div>
+                                <div>
+                                  <p class="small text-muted mb-1">Rating</p>
+                                  <p class="mb-0">8.5</p>
+                                </div>
+                              </div>
+                              <div class="d-flex pt-1">
+                                <button type="button" class="btn btn-primary flex-grow-1">Theo dõi</button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div class="testimonial-item position-relative bg-white p-5 mt-4">
-                <i style={{color:"#f65005"}} class="fa fa-quote-left fa-3x position-absolute top-0 start-0 mt-n4 ms-5"></i>
-                <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>
-                <div class="d-flex align-items-center">
-                  <img style={{ width: "50px", height: "50px",objectFit:"cover" }} class="flex-shrink-0 rounded-circle" src="https://res.cloudinary.com/sttruyen/image/upload/v1694746217/another/rhadcgcifq2qjhhnsvob.jpg" alt="" />
-                  <div class="ms-3">
-                    <h5 class="mb-1">Client Name</h5>
-                    <span>Profession</span>
-                  </div>
-                </div>
-              </div>
+              </section>
             </SwiperSlide>
           </Swiper>
         </div>
